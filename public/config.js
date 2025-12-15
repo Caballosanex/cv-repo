31cb5ed7292cdb5ce833e856a373101e4817da7e
Terraform Automation <terraform@noreply.local>
2025-12-15 16:30:21 +0100

    chore: update API endpoint [terraform-auto]
    API Endpoint: https://huo26o0ju8.execute-api.us-east-1.amazonaws.com/prod/visits
    Generated at: 2025-12-15 15:30:21 UTC

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 // Auto-generat per Terraform - NO MODIFICAR MANUALMENT
 const API_CONFIG = {
   endpoint: 'https://huo26o0ju8.execute-api.us-east-1.amazonaws.com/prod/visits',
-  generatedAt: '2025-12-15T15:13:22Z'
+  generatedAt: '2025-12-15T15:30:21Z'
 };
